28c7038a041b7118d277e5e5bff0998ecaee92a3
srameo <870830732@qq.com>
2020-12-10 11:37:18 +0800

    inner sperpixel class

--- src/image_control/test/test_control.py
+++ src/image_control/test/test_control.py
@@ -1,6 +1,7 @@
 import src.common_utils.core.image_utils as iu
 import src.common_utils.core.path_utils as pu
 from src.image_control.core.control import ImageController
+import cv2
 
 if __name__ == "__main__":
     root_path = pu.get_root_path()
@@ -9,13 +10,23 @@ if __name__ == "__main__":
     input_filename = pu.path_join("reinhard", "flowers", "red_flower_275_183.jpeg")
     input_path = pu.path_join(root_path, pu.INPUT_PATH, input_filename)
     ic = ImageController(input_path)
-    iu.print_imgs(ic.copy(),
-                  ic.cvt_LAB().copy(),
-                  ic.cvt_HSV().copy(),
-                  ic.cvt_HLS().copy(),
-                  ic.cvt_BGR().copy(),
-                  ic.cvt_RGB().copy(),
-                  ic.cvt("GRAY"))
+    slic = ic.superpixel.seeds(2, 15)
+    mask_slic = slic.getLabelContourMask()  # 获取Mask，超像素边缘Mask==1
+    label_slic = slic.getLabels()  # 获取超像素标签
+    number_slic = slic.getNumberOfSuperpixels()  # 获取超像素数目
+    mask_inv_slic = cv2.bitwise_not(mask_slic)
+    img_slic = cv2.bitwise_and(ic.ndarray, ic.ndarray, mask=mask_inv_slic)  # 在原图上绘制超像素边界
+    cv2.imshow("img_slic", img_slic)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # iu.print_imgs(ic.copy(),
+    #               ic.cvt_LAB().copy(),
+    #               ic.cvt_HSV().copy(),
+    #               ic.cvt_HLS().copy(),
+    #               ic.cvt_BGR().copy(),
+    #               ic.cvt_RGB().copy(),
+    #               ic.cvt("GRAY"))
     # ic.resize(output_path, True, 64, 64)
 
     # Test Reshape
